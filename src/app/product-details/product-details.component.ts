93032b49607b9f210fb8e9f5202036f1dc5b92fe
Shimei Wong <wshimei@gmail.com>
2018-07-18 23:29:14 +0800

    product details OK, images not

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  products: any[];
+  itemName: String;
+
+  constructor(database: AngularFireDatabase, private route: ActivatedRoute, private router: Router) {
+    database.list('/products')
+            .valueChanges()
+            .subscribe(products => {
+              this.products = products;
+            });
+   }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const itemName = params.get('itemName');
+      this.itemName = itemName;
+    });
   }
 
 }
