a67a2f1e468fbbd71ab55c15c4ddd752768eb127
Shimei Wong <wshimei@gmail.com>
2018-07-18 17:47:04 +0800

    added ngh

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'submodule', loadChildren: 'app/submodule/submodule.module#SubModule'},
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCrlf-vQezHewTxJzAmr2ynPq9reiUAILY',
+    authDomain: 'ng-firebase-dev-539cf.firebaseapp.com',
+    databaseURL: 'https://ng-firebase-dev-539cf.firebaseio.com',
+    projectId: 'ng-firebase-dev-539cf',
+    storageBucket: 'ng-firebase-dev-539cf.appspot.com',
+    messagingSenderId: '478203025900'
+  }
 };
