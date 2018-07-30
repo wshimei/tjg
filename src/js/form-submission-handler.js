c091fbd5c6aad7f274fe92f527c1385eb08e87ee
Shi Mei Wong <wshimei@mgmail.com>
2018-07-30 11:44:33 +0800

    document.ready

--- src/js/form-submission-handler.js
+++ src/js/form-submission-handler.js
@@ -1,4 +1,4 @@
-$(document).load(function () {
+$(document).ready(function () {
   (function () {
     function validEmail(email) { // see:
       var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
